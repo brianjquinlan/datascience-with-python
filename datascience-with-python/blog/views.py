daf40f09dd20097f6f66a699a592d6b2a3ec8ee4
Brian Quinlan <bquinlan8@yahoo.com>
2017-07-14 15:24:40 -0500

    move date above title on posts, basic caching on blog list and blog posts.  Still needs to have cache flushing for new content so there isnt stale caches

--- datascience-with-python/blog/views.py
+++ datascience-with-python/blog/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse, HttpResponse
 
 from django.views.generic import ListView, DetailView, ArchiveIndexView, MonthArchiveView
+from django.utils.decorators import method_decorator
 
 from .models import Post
 from taggit.models import Tag
-
 from django_comments.models import Comment
 
 from .utils import subscribe_email
 
-# Create your views here.
+# caching 
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 
 class PostListView(ListView):
     model = Post
@@ -20,9 +22,13 @@ class PostListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-       
-        recent_comments = Comment.objects.all().order_by('-id')[:3]
-        context['comments'] = recent_comments
+        
+        if cache.get('recent_comments') is not None:
+            context['comments'] = cache.get('recent_comments')
+        else:
+            recent_comments = Comment.objects.all().prefetch_related('content_object').order_by('-id')[:3]
+            cache.set('recent_comments', recent_comments)
+            context['comments'] = recent_comments
 
         return context
 
@@ -32,13 +38,26 @@ class PostListView(ListView):
         tag_slug = self.kwargs.get('tag_slug', None)
         if tag_slug:
             self.title = 'Posts tagged with %s' % tag_slug
-
+            
             tag = get_object_or_404(Tag, slug=tag_slug)
-            return qs.filter(tags__in=[tag])
+        
+            if cache.get('tagged_posts') is not None:
+                return cache.get('tagged_posts')
+            else:
+                cache.set('tagged_posts', qs.select_related('author').filter(tags__in=[tag]))
+                return qs.select_related('author').filter(tags__in=[tag])
 
         else:
-            return qs[:10]
- 
+            if cache.get('blog_posts') is not None:
+                return cache.get('blog_posts')
+            else:
+                cache.set('blog_posts', qs.select_related('author')[:5])
+                return qs.select_related('author')[:5]
+
+    @method_decorator(cache_page(60*15))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detail.html'
@@ -55,11 +74,20 @@ class PostDetailView(DetailView):
             
             post.views += 1
             post.save()
-       
-        return post
+        
+        cache_key = 'blog_' + str(post.slug)
+        if cache.get(cache_key) is not None:
+            return cache.get(cache_key)
+        else:
+            cache.set(cache_key, post)
+            return post
+    
+    @method_decorator(cache_page(60*15))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 class ArchiveIndexView(ArchiveIndexView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().select_related('author')
 
     context_object_name = 'post_list'
     date_field = 'published'
